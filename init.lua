e2719072a11e8d97714f820d72627834562702ec
kandziak <kamil.andziak@rtbhouse.com>
2024-05-12 17:29:52 +0200

    add undotree

--- init.lua
+++ init.lua
@@ -72,6 +72,9 @@ vim.opt.scrolloff = 10
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
+-- Undotree
+vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, { desc = 'Open undotree' })
+
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')

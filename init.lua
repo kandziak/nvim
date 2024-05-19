ead64369682dceafdfee1e275db570ca2aae2d6e
kandziak <kamil.andziak@rtbhouse.com>
2024-05-19 20:22:19 +0200

    add debuger

--- init.lua
+++ init.lua
@@ -503,7 +503,20 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        tsserver = {},
+        tsserver = {
+          on_attach = function()
+            local function add_missing_imports_and_save()
+              vim.lsp.buf.code_action {
+                apply = true,
+                context = {
+                  only = { 'source.addMissingImports.ts' },
+                },
+              }
+              vim.cmd 'write'
+            end
+            vim.keymap.set('n', '<Leader>I', add_missing_imports_and_save, { desc = 'Auto-import and save TS-only' })
+          end,
+        },
         --
 
         lua_ls = {
@@ -806,16 +819,7 @@ require('lazy').setup({
     end,
   },
 
-  -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
-  -- init.lua. If you want these files, they are in the repository, so you can just download them and
-  -- place them in the correct locations.
-
-  -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
-  --
-  --  Here are some example plugins that I've included in the Kickstart repository.
-  --  Uncomment any of the lines below to enable them (you will need to restart nvim).
-  --
-  -- require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
   require 'kickstart.plugins.lint',
   require 'kickstart.plugins.autopairs',
@@ -827,7 +831,6 @@ require('lazy').setup({
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
-  --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
   { import = 'custom.plugins' },
 }, {
